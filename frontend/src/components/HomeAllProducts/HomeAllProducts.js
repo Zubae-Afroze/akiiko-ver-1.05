469e1d37d6ccb1768cf6816932425078261df172
zubae <afroze@outlook.in>
2020-11-24 19:13:21 +0530

    link done

--- frontend/src/components/HomeAllProducts/HomeAllProducts.js
+++ frontend/src/components/HomeAllProducts/HomeAllProducts.js
@@ -13,13 +13,15 @@ const HomeAllProducts = (props) => {
         <div>
             <Row className='product-list-card-wrapper'>
                 {props.tablewareProductDetails.map(product => (
+                    <Link to={`/product/${product.productId}`}>
                     <div className='product-list-card-wrapper' key={product.productId}>
                         <div className='product-list-image'>
                             <img src={product.lifestyleImage} alt='home_1'/>
                             <div className='product-list-card-title'>{product.productName}</div>
-                            <div className='product-list-card-text'><Link to={`/product/${product.productId}`}>View Details - &#x20B9;{product.price}</Link></div>
+                            <div className='product-list-card-text'>View Details - &#x20B9;{product.price}</div>
                         </div>
                     </div>
+                    </Link>
                 ))}
             </Row>
         </div>
