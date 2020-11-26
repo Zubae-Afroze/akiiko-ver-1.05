185cfa6bb0860d1dbb68f2d109aa588248c84a72
chan <kchandran024@gmail.com>
2020-11-26 12:50:09 +0530

    women-bag-container-root

--- frontend/src/screens/HomeScreenWomen/HomeScreenWomen.js
+++ frontend/src/screens/HomeScreenWomen/HomeScreenWomen.js
@@ -22,16 +22,21 @@ const HomeScreenWomen = () => {
             </Row>
             <Row className='women-card-wrapper '>
                 {HomeScreenWomenProducts.map(product => (
+                   <div className='women-shop-bag-container-root'>{/* class name added here*/} 
                     <Link to={`/product/${product.productId}`}>
-                    <Col lg={2.6} key={product.productId} className='women-shop-bag-container'> {/*class name added */}
+                    <Col lg={2.6} className='women-shop-bag-container' key={product.productId} > {/*class name added */}
+                    <div>
                     <div className='women-card-image'>
                         <img src={product.heroImage} alt='women_1'/>
                         <div className='women-card-title'>{product.productName}</div>
                     <div className='women-card-text'>View Details - &#x20B9;{product.price}</div>
                     </div>
+                    </div>
                     </Col>
                     </Link>
-                ))} 
+                    </div>
+
+                    ))} 
             </Row>
             {/* <Row className='women-wrapper'>
                 <Col sm={9}>
