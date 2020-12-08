bbf5461a6dc02da83ad8a30c3515949bb25b6231
zubae <afroze@outlook.in>
2020-12-08 23:45:17 +0530

    qiuckview test

--- frontend/src/screens/HomeScreenHome/HomeScreenHome.js
+++ frontend/src/screens/HomeScreenHome/HomeScreenHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import HomeScreenHomeProducts from '../../assets/products/HomeScreenHomeProducts';
 import { Link } from 'react-router-dom';
@@ -7,11 +7,25 @@ import './HomeScreenHome.css';
 const HomeScreenHome = () => {
 
     let [imageCounter, setImageCounter] = useState(0);
+
+    let interval;
+
+    let counter = 0;
+
+    //let HomeScreenProducts = HomeScreenHomeProducts;
     
-    const handleMouseEnter = () => {
-        console.log('Hovered On Quick View');
-        setImageCounter(imageCounter + 1)
+    const handleMouseEnter = (product) => {
+        setImageCounter(1);
+        interval = setInterval(() => {
+            product.defaultImageSrc = product.images[counter % product.images.length];
+            //console.log(HomeScreenHomeProducts)
+            counter++
+        }, 1000)
     }
+
+    useEffect(() => {
+
+    })
 
     return (
         <div className='home-screen-component'>{/*container changed into div*/}
@@ -39,9 +53,9 @@ const HomeScreenHome = () => {
                             <Col className='home-card-items' key={product.productId}>
                                 <Link to={`/product/${product.productId}`}><div>
                                     <div className='home-img-wrap'>
-                                    <img className='home-card-image' src={/*product.quickView !== undefined ? product.images[imageCounter] : product.images[0]*/product.images[0]} alt='home_1'/>
+                                    <img className='home-card-image' src={product.defaultImageSrc ? product.defaultImageSrc : product.images[0]} alt='home_1'/>
                                     {product.bestSeller !== undefined ? <span className='label-best label-best-workout'>{product.bestSeller}</span> : null}
-                                    {product.quickView !== undefined ? <span className='label-view label-view-workout' onMouseEnter={handleMouseEnter}>{product.quickView}</span> : null}
+                                    {product.quickView !== undefined ? <span className='label-view label-view-workout' onMouseEnter={() => handleMouseEnter(product)}>{product.quickView}</span> : null}
                                     </div>
                                     <div className='home-card-title'>{product.productName}</div>
                                 <div className='home-card-text'>View Details - &#x20B9;{product.price !== undefined ? product.price : product.mrpPrice}</div>
